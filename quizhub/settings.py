ed97e045a15f1207dd630b43b740c61b84537702
Sajid Kalam <sajidkalam@proton.me>
2025-11-02 22:32:32 +0530

    production in your settings

--- quizhub/settings.py
+++ quizhub/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
-GEMINI_API_KEY = config('GEMINI_API_KEY')
+GEMINI_API_KEY = config('GEMINI_API_KEY', default='')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
